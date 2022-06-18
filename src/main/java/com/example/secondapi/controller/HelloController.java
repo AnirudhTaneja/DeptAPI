d026574da834d26d210d60ace1802ee1df381aab
AnirudhTaneja <Anirudh.Taneja@target.com>
2022-06-18 18:22:31 -0400

    fetch values from config file

--- src/main/java/com/example/secondapi/controller/HelloController.java
+++ src/main/java/com/example/secondapi/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.secondapi.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
     @GetMapping("/")
     public String HelloWorld(){
-        return "Hello Second APIssss!!";
+        return welcomeMessage;
     }
 
 }
